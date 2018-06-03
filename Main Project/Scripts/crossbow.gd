f48178412d71e8c6221cf5a03fc2eb03f708ec35
Swannicus <swan@thegoodsirs.com>
2018-06-02 20:29:17 -0400

    its crossbow time

--- Main Project/Scripts/crossbow.gd
+++ Main Project/Scripts/crossbow.gd
@@ -6,14 +6,18 @@ func _ready():
 
 	pass
 
-func _process(delta):
-	$"..".look_at(get_global_mouse_position())
-	if get_global_mouse_position().x-self.global_position.x < 0 :
-		if self.scale != Vector2(1,-1):
-			self.apply_scale(Vector2(1,-1))
-	if get_global_mouse_position().x-self.global_position.x > 0 :
-		if self.scale != Vector2(1,1):
-			self.apply_scale(Vector2(1,-1))
+#func _process(delta):
+
+
+func _physics_process(delta):
+	if get_parent().get_parent().controlBoolean:
+		$"..".look_at(get_global_mouse_position())
+		if get_global_mouse_position().x-self.global_position.x < 0 :
+			if self.scale != Vector2(1,-1):
+				self.apply_scale(Vector2(1,-1))
+		if get_global_mouse_position().x-self.global_position.x > 0 :
+			if self.scale != Vector2(1,1):
+				self.apply_scale(Vector2(1,-1))
 
 func attack():
 	var bolt = boltscene.instance()
@@ -21,4 +25,11 @@ func attack():
 	bolt.setup(angle.normalized())
 	get_parent().get_parent().get_parent().add_child(bolt)
 	bolt.global_position = $Sprite/muzzle.global_position
+	rpc("remote_attack",get_parent().get_parent().player_id,get_global_mouse_position())
 
+func remote_attack(id,target):
+	var bolt = boltscene.instance()
+	var angle = target - $Sprite/muzzle.global_position
+	bolt.setup(angle.normalized())
+	get_parent().get_parent().get_parent().add_child(bolt)
+	bolt.global_position = $Sprite/muzzle.global_position
